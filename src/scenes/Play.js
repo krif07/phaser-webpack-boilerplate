013bbc7f0cebf77bc44b1554392f278b5eab7dbf
cfdavila <cfdavila@indracompany.com>
2021-10-18 16:22:30 -0500

    layouts

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,15 +4,24 @@ class Play extends Phaser.Scene {
 
     constructor() {
         super('PlayScene');
-        //this.config = config;
-    }
-
-    preload(){
-        this.load.image('sky', 'assets/sky.png');
     }
 
     create() {
-        this.add.image(0, 0, 'sky').setOrigin(0);
+        const map = this.createMap();
+        const layers = this.createLayers(map);
+    }
+
+    createMap(){
+        const map = this.make.tilemap({key: 'map'});
+        map.addTilesetImage('main_lev_build_1','tiles-1');
+        return map;
+    }
+
+    createLayers(map){
+        const tileSet1 = map.getTileset('main_lev_build_1');
+        const environment = map.createStaticLayer('environment', tileSet1);
+        const platforms = map.createDynamicLayer('platforms', tileSet1);
+        return {environment, platforms}
     }
 }
 
