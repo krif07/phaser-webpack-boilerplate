f83f541d49993d4d0fecfbc8aef7d58892435f61
cfdavila <cfdavila@indracompany.com>
2021-10-18 22:43:50 -0500

    Player and collides

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -9,6 +9,8 @@ class Play extends Phaser.Scene {
     create() {
         const map = this.createMap();
         const layers = this.createLayers(map);
+        const player = this.createPlayer();
+        this.physics.add.collider(player, layers.platformsColliders);
     }
 
     createMap(){
@@ -19,9 +21,20 @@ class Play extends Phaser.Scene {
 
     createLayers(map){
         const tileSet1 = map.getTileset('main_lev_build_1');
+        const platformsColliders = map.createDynamicLayer('platforms_colliders', tileSet1);
+        const platforms = map.createStaticLayer('platforms', tileSet1);
         const environment = map.createStaticLayer('environment', tileSet1);
-        const platforms = map.createDynamicLayer('platforms', tileSet1);
-        return {environment, platforms}
+
+        //platformsColliders.setCollisionByExclusion(-1, true);
+        platformsColliders.setCollisionByProperty({collides: true});
+        return {environment, platforms, platformsColliders}
+    }
+
+    createPlayer(){
+        const player = this.physics.add.sprite(100, 250, 'player');
+        player.body.setGravityY(500);
+        player.setCollideWorldBounds(true);
+        return player;
     }
 }
 
